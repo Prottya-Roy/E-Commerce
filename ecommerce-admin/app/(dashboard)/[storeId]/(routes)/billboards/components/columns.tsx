0c9e866120de103e03c6dbba413d0806aae57c49
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-06-10 15:47:09 +0600

    pagination, sorting, filter added to the table column through shadcn

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -1,21 +1,33 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
 
 
 export type BillboardColumn = {
-  id: string
-  label: string
-  createdAt: string
+    id: string
+    label: string
+    createdAt: string
 }
 
 export const columns: ColumnDef<BillboardColumn>[] = [
-  {
-    accessorKey: "label",
-    header: "Label",
-  },
-  {
-    accessorKey: "createdAt",
-    header: "Date",
-  },
+    {
+        accessorKey: "label",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Label
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "createdAt",
+        header: "Date",
+    },
 ]
