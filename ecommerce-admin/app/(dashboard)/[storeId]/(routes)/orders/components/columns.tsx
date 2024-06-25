72c7994c0c0270d927cfeb94a77e7fef805e3176
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-06-25 12:10:43 +0600

    Orders module done
    Order model, order Item model, order column and navigation added

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { CellAction } from "./cell-action"
+
+
+export type OrderColumn = {
+    id: string
+    phone: string
+    address: string
+    isPaid: Boolean
+    totalPrice: String
+    products: String
+    createdAt: string
+}
+
+export const columns: ColumnDef<OrderColumn>[] = [
+    {
+        accessorKey: "products",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Products
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "phone",
+        header: "Phone"
+    },
+    {
+        accessorKey: "address",
+        header: "Address"
+    },
+    {
+        accessorKey: "totalPrice",
+        header: "Total Price"
+    },
+    {
+        accessorKey: "isPaid",
+        header: "Paid"
+    },
+    {
+        accessorKey: "createdAt",
+        header: "Date",
+    }
+]
