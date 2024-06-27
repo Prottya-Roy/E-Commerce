5248af795ae77c0bac83e193c77b73efd4b0df6d
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-06-27 12:43:19 +0600

    featured product list, product card currency added and working

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,16 +1,22 @@
 import getBillboard from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
 
+    const products = await getProducts({ isFeatured: true });
     const billboard = await getBillboard("064bf8d7-c206-4645-b5d8-8ac1588476f2");
     return (
         <Container>
             <div className="space-y-10 pb-10">
                 <Billboard data={billboard}/>
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title= "Featured Products" items={products} />
             </div>
         </Container>
     );
