5b1a98726017c1bce6392ffae3f8feb771cc451b
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-06-30 15:49:59 +0600

    gallery tab and gallery page added along with initial structure

---
+++ ecommerce-store/components/gallery/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Image from "next/image";
+import { TabGroup, TabList } from "@headlessui/react";
+import { Image as ImageType } from "@/types";
+import React from "react";
+import GalleryTab from "./gallery-tab";
+
+interface GalleryProps {
+    images: ImageType[];
+};
+
+const Gallery: React.FC<GalleryProps> = ({
+    images
+}) => {
+    return (
+        <TabGroup as="div" className="flex flex-col-reverse">
+            <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+                <TabList className="grid grid-cols-4 gap-6">
+                    {images.map((image) => (
+                        <GalleryTab key={image.id} image= {image} />
+                    ))}
+                </TabList>
+            </div>
+        </TabGroup>
+    )
+}
+
+export default Gallery;
