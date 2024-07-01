5d58095659d389376471b8b024015c647cce6dda
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-07-01 11:11:11 +0600

    Colors Column cell update for admin colorColumn

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
@@ -30,7 +30,13 @@ export const columns: ColumnDef<ColorColumn>[] = [
     },
     {
         accessorKey: "value",
-        header: "Value"
+        header: "Value",
+        cell: ({row}) => (
+            <div className="flex items-center gap-x-2">
+                {row.original.value}
+                <div className="h-6 w-6 rounded-full border border-gray-600" style={{backgroundColor: row.original.value}} />
+            </div>
+        )
     },
     {
         accessorKey: "createdAt",
