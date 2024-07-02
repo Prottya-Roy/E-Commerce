dc404f597df86a60fb374b1acb09c862053a7505
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-07-02 14:53:33 +0600

    cart empty checkl

--- ecommerce-store/app/(routes)/cart/page.tsx
+++ ecommerce-store/app/(routes)/cart/page.tsx
@@ -23,12 +23,15 @@ const CartPage = () => {
                     <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
                     <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
                         <div className="lg:col-span-7">
-                            {cart.items.map((item) => (
-                                <CartItem
-                                    key={item.id}
-                                    data={item}
-                                />
-                            ))}
+                            {cart.items.length === 0 && <p className="text-neutral-500">Np Items Added to the Cart</p>}
+                            <ul>
+                                {cart.items.map((item) => (
+                                    <CartItem
+                                        key={item.id}
+                                        data={item}
+                                    />
+                                ))}
+                            </ul>
                         </div>
                     </div>
                 </div>
