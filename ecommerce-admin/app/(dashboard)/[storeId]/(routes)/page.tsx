7bd1233d15dfb40b0b2393f3dfecec4e653aa8ff
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-08-01 15:00:49 +0600

    total revenue added

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,10 @@
-import { Card, CardHeader } from "@/components/ui/card";
+import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
+import { CreditCard, DollarSign, Package } from "lucide-react";
 
 interface DashboardPageProps {
     params: {storeId: string}
@@ -10,6 +13,12 @@ interface DashboardPageProps {
 const DashboardPage: React.FC<DashboardPageProps> = async({
     params
 }) => {
+    const totalRevenue = await getTotalRevenue(params.storeId);
+
+    const salesCount = () => {};
+    const stockCount = () => {};
+
+
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
@@ -17,9 +26,43 @@ const DashboardPage: React.FC<DashboardPageProps> = async({
                 <Separator />
                 <div className="grid gap-4 grid-cols-3">
                     <Card>
-                        <CardHeader className="text-sm font-medium">
-                            Total Revenue
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Total Revenue
+                            </CardTitle>
+                            <DollarSign className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                {formatter.format(totalRevenue)}
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Sales
+                            </CardTitle>
+                            <CreditCard className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                +25
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Products in Stock
+                            </CardTitle>
+                            <Package className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                12
+                            </div>
+                        </CardContent>
                     </Card>
                 </div>
             </div>
