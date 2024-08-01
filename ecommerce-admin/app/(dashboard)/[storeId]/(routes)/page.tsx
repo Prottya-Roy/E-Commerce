fd2217d6503c2e5fbf2ba7545aee99a6aae7f32a
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-08-01 12:30:10 +0600

    card added for admin dashboard

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,6 @@
+import { Card, CardHeader } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
 
 interface DashboardPageProps {
@@ -7,16 +10,19 @@ interface DashboardPageProps {
 const DashboardPage: React.FC<DashboardPageProps> = async({
     params
 }) => {
-
-    const store = await prismadb.store.findFirst({
-        where: {
-            id: params.storeId
-        }
-    });
-
     return (
-        <div>
-            Active Store : {store?.name};
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <Heading title="Dashboard" description="Overview of your store" />
+                <Separator />
+                <div className="grid gap-4 grid-cols-3">
+                    <Card>
+                        <CardHeader className="text-sm font-medium">
+                            Total Revenue
+                        </CardHeader>
+                    </Card>
+                </div>
+            </div>
         </div>
     );
 }
