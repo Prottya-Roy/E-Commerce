847cce7278f9d3a051dca267e974b113b8ded3bb
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-08-04 11:27:14 +0600

    Theme toggle added

--- ecommerce-admin/components/navbar.tsx
+++ ecommerce-admin/components/navbar.tsx
@@ -4,6 +4,7 @@ import StoreSwitcher from "./store-switcher";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "./ui/theme-toggle";
 
 const Navbar = async () => {
     const { userId } = auth();
@@ -23,6 +24,7 @@ const Navbar = async () => {
                 <StoreSwitcher items={stores} />
                 <MainNav className="mx-6" />
                 <div className="ml-auto flex items-center space-x-4">
+                    <ThemeToggle />
                     <UserButton afterSignOutUrl="/" />
                 </div>
 
